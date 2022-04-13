2a0d0c5cf180c28aa37e7dfe6941fa866f7ea423
Rafael Kowalski <rafaelmk06@gmail.com>
2022-04-12 21:30:31 -0300

    criando os primeiros testes de movimentacao

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -8,45 +8,31 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(Cor.Preta, tab), new Posicao(0, 7));
-            tab.colocarPeca(new Cavalo(Cor.Preta, tab), new Posicao(0, 1));
-            tab.colocarPeca(new Cavalo(Cor.Preta, tab), new Posicao(0, 6));
-            tab.colocarPeca(new Bispo(Cor.Preta, tab), new Posicao(0, 2));
-            tab.colocarPeca(new Bispo(Cor.Preta, tab), new Posicao(0, 5));
-            tab.colocarPeca(new Dama(Cor.Preta, tab), new Posicao(0, 3));
-            tab.colocarPeca(new Rei(Cor.Preta, tab), new Posicao(0, 4));
-            tab.colocarPeca(new Peao(Cor.Preta, tab), new Posicao(1, 0));
-            tab.colocarPeca(new Peao(Cor.Preta, tab), new Posicao(1, 1));
-            tab.colocarPeca(new Peao(Cor.Preta, tab), new Posicao(1, 2));
-            tab.colocarPeca(new Peao(Cor.Preta, tab), new Posicao(1, 3));
-            tab.colocarPeca(new Peao(Cor.Preta, tab), new Posicao(1, 4));
-            tab.colocarPeca(new Peao(Cor.Preta, tab), new Posicao(1, 5));
-            tab.colocarPeca(new Peao(Cor.Preta, tab), new Posicao(1, 6));
-            tab.colocarPeca(new Peao(Cor.Preta, tab), new Posicao(1, 7));
-            tab.colocarPeca(new Torre(Cor.Branca, tab), new Posicao(7, 0));
-            tab.colocarPeca(new Torre(Cor.Branca, tab), new Posicao(7, 7));
-            tab.colocarPeca(new Cavalo(Cor.Branca, tab), new Posicao(7, 1));
-            tab.colocarPeca(new Cavalo(Cor.Branca, tab), new Posicao(7, 6));
-            tab.colocarPeca(new Bispo(Cor.Branca, tab), new Posicao(7, 2));
-            tab.colocarPeca(new Bispo(Cor.Branca, tab), new Posicao(7, 5));
-            tab.colocarPeca(new Dama(Cor.Branca, tab), new Posicao(7, 3));
-            tab.colocarPeca(new Rei(Cor.Branca, tab), new Posicao(7, 4));
-            tab.colocarPeca(new Peao(Cor.Branca, tab), new Posicao(6, 0));
-            tab.colocarPeca(new Peao(Cor.Branca, tab), new Posicao(6, 1));
-            tab.colocarPeca(new Peao(Cor.Branca, tab), new Posicao(6, 2));
-            tab.colocarPeca(new Peao(Cor.Branca, tab), new Posicao(6, 3));
-            tab.colocarPeca(new Peao(Cor.Branca, tab), new Posicao(6, 4));
-            tab.colocarPeca(new Peao(Cor.Branca, tab), new Posicao(6, 5));
-            tab.colocarPeca(new Peao(Cor.Branca, tab), new Posicao(6, 6));
-            tab.colocarPeca(new Peao(Cor.Branca, tab), new Posicao(6, 7));
+                while (!partida.Terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.Tab);
 
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-            Tela.imprimirTabuleiro(tab);
+                    partida.executaMovimento(origem, destino);
+                }
 
-            Console.ReadLine();
+                
+            }
+            catch (TabuleioException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+         
         }
     }
 }
---
+++ JogoDeXadrez/xadrez/PartidaDeXadrez.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using tabuleiro;
+
+namespace tabuleiro.xadrez
+{
+    public class PartidaDeXadrez
+    {
+        public Tabuleiro Tab { get; private set; }
+        private int Turno;
+        private Cor JogadorAtual;
+        public bool Terminada { get; private set; }
+
+        public PartidaDeXadrez()
+        {
+            Tab = new Tabuleiro(8, 8);
+            Turno = 1;
+            JogadorAtual = Cor.Branca;
+            Terminada = false;
+            colocarPecas();
+        }
+
+        public void executaMovimento(Posicao origem, Posicao destino)
+        {
+            Peca p = Tab.retirarPeca(origem);
+            p.incrementarQteMovimentos();
+            Peca pecaCapturada = Tab.retirarPeca(destino);
+            Tab.colocarPeca(p, destino);
+        }
+
+        private void colocarPecas()
+        {
+
+            Tab.colocarPeca(new Torre(Cor.Preta, Tab), new PosicaoXadrez('c',1).toPosicao());
+            Tab.colocarPeca(new Torre(Cor.Branca, Tab), new PosicaoXadrez('b', 2).toPosicao());
+        }
+    }
+}
